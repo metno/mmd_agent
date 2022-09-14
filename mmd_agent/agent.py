f06e36f5b5da30be5e671f266fb8a6003849c8b5
Johannes Tobiassen Langvatn <johannestl@met.no>
2022-09-14 10:39:08 +0200

    Refactoring to conform to other S-ENDA repos

---
+++ mmd_agent/agent.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""
+MMD_AGENT : Main MMD agent
+==================
+
+Copyright 2021 MET Norway
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import os
+import requests
+
+
+def validate_mmd(mmd):
+    url = 'https://dmci-staging.s-enda.k8s.met.no/v1/validate'
+    response = requests.post(url, data=mmd)
+    return response.status_code == 200
+
+
+def send_to_dmci(mmd):
+    print("Sending to dmci ")
+    response = requests.post(
+        'https://dmci-staging.s-enda.k8s.met.no/v1/insert', data=mmd
+        )
+    return response.status_code
+
+
+def main(incoming_mmd):
+    if incoming_mmd is not None and incoming_mmd != "":
+        mmd = incoming_mmd.encode()
+        if validate_mmd(mmd):
+            send_to_dmci(mmd)
+        else:
+            print("invalid mmd")
+    else:
+        print("product event mmd is none or empty  ")
+
+
+if __name__ == "__main__":
+    incoming_mmd = os.environ.get("MMS_PRODUCT_EVENT_MMD", None)
+    main(incoming_mmd)
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,15 +1,48 @@
-import unittest
-from unittest.mock import MagicMock, patch
-from script import send_to_dmci,validate_mmd
+"""
+MMD_AGENT : MMD Agent Test
+==================
 
+Copyright 2021 MET Norway
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import pytest
+import requests
+
+from mmd_agent.agent import send_to_dmci, validate_mmd
+
+@pytest.mark.mmd_agent
+def testMMDAgentAgentValidate_mmd(monkeypatch):
+    class mockResp:
+        text = "Mock Respone"
+        status_code = 200
+
+    with monkeypatch.context() as mp:
+        mp.setattr(requests, "post", lambda *a,**k: mockResp)
+        assert validate_mmd("") is True
+
+
+
+"""
 class TestScript(unittest.TestCase):
 
-    
+
     def test_incoming_mmd_is_not_valid(self):
         self.assertFalse(validate_mmd("asdfA1qw"))
 
 
-    @patch('script.requests')    
+    @patch('script.requests')
     def test_validate_mmd_succes(self,mock_requests):
 
         mock_response=MagicMock()
@@ -18,7 +51,7 @@ class TestScript(unittest.TestCase):
         mock_requests.post.return_value=mock_response
         self.assertEqual(validate_mmd("mms"),True)
 
-    @patch('script.requests')    
+    @patch('script.requests')
     def test_validate_mmd_fails(self,mock_requests):
 
         mock_response=MagicMock(status_code=500)
@@ -26,7 +59,7 @@ class TestScript(unittest.TestCase):
         mock_requests.post.return_value=mock_response
         self.assertEqual(validate_mmd("mms"),False)
 
-    @patch('script.requests')    
+    @patch('script.requests')
     def test_send_to_dmci_succes(self,mock_requests):
 
         mock_response=MagicMock()
@@ -35,13 +68,14 @@ class TestScript(unittest.TestCase):
         mock_requests.post.return_value=mock_response
         self.assertEqual(send_to_dmci("mms"),200)
 
-    @patch('script.requests')    
+    @patch('script.requests')
     def test_send_to_dmci_fails(self,mock_requests):
 
         mock_response=MagicMock(status_code=500)
         mock_response.text="Invalid mmd"
         mock_requests.post.return_value=mock_response
         self.assertEqual(send_to_dmci("mms"),500)
-      
+
 if __name__=='__script__':
     unittest.script()
+"""
